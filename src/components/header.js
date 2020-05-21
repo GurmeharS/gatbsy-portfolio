0d22150cc1b83e33271b2d74528caf0985a99e41
GurmeharS <gurmehar.sandhu@mail.utoronto.ca>
2020-05-21 18:12:42 -0400

    Final (for now) header

--- src/components/header.js
+++ src/components/header.js
@@ -2,36 +2,49 @@ import { Link } from "gatsby"
 import React, { useState, useEffect, useRef } from "react"
 import styled from "styled-components"
 
+import * as constants from "./constant"
+
+const size = constants.device;
+
 const Navbar = styled.div`
   display: flex;
   width: 75vw;
-  height: 10vh;
+  height: 8vh;
   opacity: 1;
-  justify-content: center;
+  justify-content: space-between;
   align-items: center;
   background-color: #6F6CFF;
   opacity: 1;
+  font-size: 30px;
+  border-radius: 15px;
+  @media ${size.mobileXL} {
+    height: 5vh;
+    font-size: 15px;
+  }
 `;
 
 // Wrapper
 const StyledHeader = styled.header`
   position: sticky;
+  z-index: 10000;
   top: 0;
   display: flex;
   align-items: center;
   justify-content: center;
 `;
 
+const linkStyle = {
+  margin: 'auto',
+  color: 'white'
+};
+
+
 const InnerHeader = () => (
   <Navbar>
-    <div className="logo">
-      <Link style={{ color: 'white' }} to="/">Logo</Link>
-    </div>
-    <div className="navigation">
-      <Link style={{ color: 'white' }} to="/about">About</Link>
-      <Link style={{ color: 'white' }} to="/projects">Projects</Link>
-      <Link style={{ color: 'white' }} to="/experience">Experience</Link>
-    </div>
+    <Link style={linkStyle} to="/">Logo</Link>
+    <Link style={linkStyle} to="/about">About</Link>
+    <Link style={linkStyle} to="/projects">Projects</Link>
+    <Link style={linkStyle} to="/experience">Experience</Link>
   </Navbar>
 );
 
